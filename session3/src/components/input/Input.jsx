7505581b47aca4ecb8e468c31813e2b85c1e8935
Melik <melikpapikyan@gmail.com>
2025-07-28 13:45:12 +0400

    Logic for the input has been added to the dashboard, and CSS bugs have been fixed

--- session3/src/components/input/Input.jsx
+++ session3/src/components/input/Input.jsx
@@ -1,22 +1,41 @@
+import { useState } from 'react'
 import './input.css';
 
 const Input = () => {
+    const [value, setValue] = useState('');
+    const [visible, setVisible] = useState(true);
+
+    const inputChanges = (e) => {
+        const target = e.target.value;
+        setValue(target);
+    }
+
+    const toggleBtn = () => {
+        setVisible((prev) => !prev)
+    }
+
+    const displayedTxt = !visible
+        ? `Text is hidden (symbols: ${value.length})`
+        : value;
+
     return (
         <div className="input__box">
-            <h3>interactive text with toggle switch</h3>
+            <h3>Interactive text with toggle switch</h3>
             <div className="box__input">
                 <label htmlFor="inp">Enter text:</label>
-                <input id='inp' type="text" />
+                <input onChange={inputChanges} value={value} id='inp' type="text" />
             </div>
             <div className="box__btn">
-                <button>hide text</button>
+                <button onClick={toggleBtn}>
+                    {visible ? 'Hide text' : 'Show Text'}
+                </button>
                 <span>
-                    simbols: <span className="text__counter">0</span>
+                    symbols: <span className="text__counter">{value.length}</span>
                 </span>
             </div>
             <blockquote>
-                <p>your text:</p>
-                <p className='input__text'>hellow how are you</p>
+                <p>Your text:</p>
+                <p className='input__text'>{displayedTxt}</p>
             </blockquote>
         </div>
     )
