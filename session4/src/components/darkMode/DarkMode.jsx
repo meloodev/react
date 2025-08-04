3ac7881b46802e90133f3a076b6e96c278dcdeec
Melik <melikpapikyan1@gmail.com>
2025-08-04 21:25:38 +0400

    Dark theme added

--- session4/src/components/darkMode/DarkMode.jsx
+++ session4/src/components/darkMode/DarkMode.jsx
@@ -1,9 +1,52 @@
+import { useState, useEffect } from 'react';
 import './darkMode.css';
+
+
 const DarkMode = () => {
+
+    const [dark, setDark] = useState(false);
+
+    useEffect(() => {
+        const saved = localStorage.getItem('formMode');
+        if (!saved) return;
+
+        const { theme } = JSON.parse(saved);
+        if (theme === 'dark') {
+            setDark(true);
+            document.body.classList.add('dark');
+        }
+    }, []);
+
+    const theme = () => {
+        setDark(prev => {
+            const newValue = !prev;
+
+            if (newValue) {
+                saveTheme('dark');
+                document.body.classList.add('dark');
+            } else {
+                localStorage.removeItem('formMode');
+                document.body.classList.remove('dark');
+            }
+
+            return newValue;
+        });
+    };
+
+    const saveTheme = (mode) => {
+        const currentSettings = JSON.parse(localStorage.getItem('formMode')) || {};
+        currentSettings.theme = mode;
+        localStorage.setItem('formMode', JSON.stringify(currentSettings));
+    }
+
+
+
+
+
     return (
         <div className="tumb_two">
             <label className="wrap_two">
-                <input className="inp2" type="checkbox" />
+                <input className="inp2" type="checkbox" checked={dark} onChange={theme} />
                 <span className="btn2"></span>
             </label>
         </div>
